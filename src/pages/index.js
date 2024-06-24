11a72e194c359f793ff0c7533f1ca429a44ce49d
Isaac Samuel Lopez <isaaclopezprog@gmail.com>
2024-06-24 12:26:37 -0400

    agregado del SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,19 @@ import Isaias61 from '@/components/isaias61';
 import Mateo4 from '@/components/mateo4';
 import Galeria from '@/components/gallery';
 
+import { NextSeo } from 'next-seo';
+
 
 
 export default function Home() {
 
   return (
     <>
+
+      <NextSeo
+        title="Ministerio International Jesus Pan de Vida"
+        canonical="https://www.ministeriointjesuspandevida.com/"
+        />
       <Layout>
         <Image />
         <Hacemos />
